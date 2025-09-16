3d7edaa3119f9c7b64951d3c6c37a54dcc967ac7
progmax646 <maksim.r@bimiboo.com>
2025-09-16 20:59:35 +0500

    fix price tag

--- src/OurMenu.tsx
+++ src/OurMenu.tsx
@@ -16,25 +16,32 @@ const OurMenu = () => {
     return (
         <section className="our-menu">
             <div className="menu-image">
-            <div className="container">
+                <div className="container">
                     <div className="our-menu__header"><h2>Наше меню</h2></div>
-                <div className="our-menu__inner">
+                    <div className="our-menu__inner">
 
-                    {burgers.map((item, index) => (
-                        <div className='burger-item' key={index}>
-                        <div className={item.recommended ? 'isRecommended' : 'not-recommended'}><p>RECOMMENDED</p></div>
-                        <img src={item.icon} alt=""/>
-                            <div className='price-tag'>
-                        <h3>{item.price}</h3>
-                            </div>
-                        <h2>Гамбургер макси</h2>
-                        <p>Максимально толстый
-                            слой мяса</p>
-                        <button className='btn dark-orange-btn'>ЗАКАЗАТЬ</button>
-                    </div>))}
+                        {burgers.map((item, index) => (
+                            <div className='burger-item' key={index}>
+                                <div className={item.recommended ? 'isRecommended' : 'not-recommended'}>
+                                    <p>RECOMMENDED</p></div>
+                                <div className='burger-item__img'>
+                                    <img src={item.icon} alt=""/>
+                                    <div className='price-tag'>
+                                        <div className='price-tag__inner'>
+                                            <h3>
+                                                {item.price}
+                                            </h3>
+                                        </div>
+                                    </div>
+                                </div>
+                                <h2>Гамбургер макси</h2>
+                                <p>Максимально толстый
+                                    слой мяса</p>
+                                <button className='btn dark-orange-btn'>ЗАКАЗАТЬ</button>
+                            </div>))}
+                    </div>
+
                 </div>
-
-            </div>
             </div>
         </section>
     );
