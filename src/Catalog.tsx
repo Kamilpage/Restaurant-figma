523ec0fb992eab467fb24499b72a03738f9d9b82
Kamilpage <115654620+Kamilpage@users.noreply.github.com>
2025-09-21 17:24:39 +0500

    some changes, adding zoom images

--- src/Catalog.tsx
+++ src/Catalog.tsx
@@ -1,6 +1,7 @@
 import Header from "./UI components/Header.tsx";
 import {useState, useEffect} from "react";
 import ItemService from "./API/Fetching.tsx";
+import ZoomImg from "./ZoomImg.tsx";
 
 
 const Catalog = () => {
@@ -25,8 +26,9 @@ const Catalog = () => {
             <h1>Menu</h1>
             <div className='catalog-content'>
                 {items.map((item,index) => (<div key={index} className='menu-options'>
-                    <img src={item.image} alt=""/>
+                   <ZoomImg src={item.image} alt={item.title} price={item.price} />
                     <h2>{item.title}</h2>
+                    <h2>{item.price}$</h2>
                     <div>
 
                     <button className='btn orange-btn'>Add +1</button>
--- src/ZoomImg.tsx
+++ src/ZoomImg.tsx
@@ -1,12 +1,31 @@
 import Modal from "./UI components/Modal.tsx";
+import {useState} from "react";
 
-const ZoomImg = () => {
+const ZoomImg = ({src, alt, price}) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const openModal = () => setIsOpen(true);
+    const closeModal = () => setIsOpen(false);
     return (
-        <div className='container'>
-<div>
-    <img src="" alt=""/>
-</div>            
-        </div>
+        <section>
+            <div>
+                <img src={src} alt={alt}
+                     onClick={openModal}
+                />
+            </div>
+            <Modal isOpen={isOpen} onClose={closeModal}>
+                <div onClick={closeModal} className='zoom-img'>
+                <img
+                    src={src}
+                    alt={alt}
+                    onClick={(e)=>e.stopPropagation()}
+                />
+                    <div className='img-info'>
+                    <p>{alt}</p>
+                    <p>{price}$</p>
+                    </div>
+                </div>
+                </Modal>
+        </section>
     );
 };
 
